e4ad4ec7ae4c25f45f09989397a55065abd9a346
Juhana Kekoni <juhanakekoni@yahoo.com>
2019-05-11 19:32:16 +0900

    Add StartMenu script to control StarState
    Pressing space takes you to the next state

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    private Animator _state_controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _state_controller = transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _state_controller.SetTrigger("Game");
+        }
+    }
+}
