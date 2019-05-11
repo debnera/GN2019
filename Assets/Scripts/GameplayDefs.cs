1ef87c63dbc0cd3ddd2c3acc0b08d2f0ac7e00f0
Juhana Kekoni <juhanakekoni@yahoo.com>
2019-05-12 04:54:17 +0900

    Add GameplayDefs
    Contains data types fro storing character information

---
+++ Assets/Scripts/GameplayDefs.cs
@@ -0,0 +1,24 @@
+﻿
+public enum EnemyType
+{
+    Wolf,
+    Cloud,
+    Typhoon,
+    Boulder,
+}
+
+internal class CharacterDef
+{
+    public CharacterDef(string prefab, string audio, EnemyType enemy)
+    {
+        prefabName = prefab;
+        audioName = audio;
+        counteredEnemy = enemy;
+    }
+
+    public string prefabName;
+    public string audioName;
+    public EnemyType counteredEnemy;
+}
+
+
