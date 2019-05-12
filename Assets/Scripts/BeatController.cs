0c38ad343e2f7fe8e1916e65869f415597f29315
tohtoriteppo <tohtoriteppo@hotmail.com>
2019-05-12 13:18:18 +0300

    animation

--- Assets/Scripts/BeatController.cs
+++ Assets/Scripts/BeatController.cs
@@ -16,6 +16,9 @@ public class BeatController : MonoBehaviour
     public float countdown_beat { get; private set; } = 0;
     private const float beat_length = hits / (bpm / 60);
 
+    public float countdown_halfBeat { get; private set; } = 0;
+
+    private bool overHalf = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,16 +28,14 @@ public class BeatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        countdown_loop -= Time.deltaTime;
-        if (countdown_loop < 0)
+        
+
+        countdown_halfBeat -= Time.deltaTime;
+        if (countdown_halfBeat < 0)
         {
-            countdown_loop = loop_length;
-            foreach (var character in FindObjectsOfType<Character>())
-            {
-                var audio = character.GetComponent<AudioSource>();
-                audio.Stop();
-                audio.Play();
-            }
+            Debug.Log("halfBeat");
+            countdown_halfBeat = beat_length*0.5f;
+            Animate();
         }
 
         countdown_beat -= Time.deltaTime;
@@ -42,6 +43,29 @@ public class BeatController : MonoBehaviour
         {
             Debug.Log("Beat");
             countdown_beat = beat_length;
+        }
+
+        countdown_loop -= Time.deltaTime;
+        if (countdown_loop < 0)
+        {
+            countdown_loop = loop_length;
+            countdown_beat = beat_length;
+            countdown_halfBeat = beat_length * 0.5f;
+            foreach (var character in FindObjectsOfType<Character>())
+            {
+                var audio = character.GetComponent<AudioSource>();
+                audio.Stop();
+                audio.Play();
+            }
+        }
+    }
+
+    private void Animate()
+    {
+        SpriteSwapper[] swaps = FindObjectsOfType<SpriteSwapper>();
+        foreach (SpriteSwapper swap in swaps)
+        {
+            swap.SwapImage();
         }
     }
 
